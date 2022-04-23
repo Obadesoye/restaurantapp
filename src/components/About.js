8926493520d530312b66dba13fdd30c36c7d2000
obadesoye <adesoyeadebiyi1@gmail.com>
2022-04-23 04:14:15 -0600

    modified

--- src/components/About.js
+++ src/components/About.js
@@ -1,8 +1,16 @@
 import React from 'react'
+import "./styles/about.css";
 
-const About = () => {
-  return (
-    <div></div>
+function About() {
+
+  return(
+    <div>
+      <h2>Mission</h2>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta explicabo minus provident dicta quaerat quos. Corrupti atque totam reiciendis sapiente soluta velit illo laboriosam quae pariatur fugiat. Tempore, autem ab.</p>
+
+      <h2>Vision</h2>
+      <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ut quibusdam perferendis obcaecati? Voluptatum voluptatibus, rem, ex alias minima assumenda excepturi quibusdam quidem dolores hic voluptates similique debitis impedit delectus autem?</p>
+    </div>
   )
 }
 
